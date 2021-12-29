4b62d89dbd70731e2ec02fb078b67b3615404602
victor <lnvictor@github.com>
2021-12-28 21:45:14 -0300

    Email Scheduled implemented

--- project/blueprints/mail.py
+++ project/blueprints/mail.py
@@ -1,16 +1,32 @@
-import smtplib
-
-from flask import Blueprint, Response, request
 import json
-import ipdb
 
-from facade.celery_service import send_email_to_mom
+from flask import Blueprint, request
+
+from facade.celery_service import schedule_service, send_email_to_mom
 
 mail_endpoints = Blueprint("mail_endpoints", __name__)
 
+
 @mail_endpoints.route("/send_mail", methods=["POST"])
 def send():
-    # ipdb.sset_trace()
     data = json.loads(request.data)
-    send_email_to_mom.delay(data["origin"], data["destiny"], data["pwd"], data["message"])
-    return Response({"message": "Mail sended!!"}, status=200)
+    send_email_to_mom.delay(
+        data["origin"], data["destiny"], data["pwd"], data["message"]
+    )
+    return {"message": "Mail sended!!"}
+
+
+@mail_endpoints.route("/schedule_mail", methods=["POST"])
+def schedule():
+    data = json.loads(request.data)
+    schedule_service(
+        data["origin"],
+        data["destiny"],
+        data["pwd"],
+        data["message"],
+        data["day"],
+        data["hour"],
+        data["minute"],
+    )
+
+    return {"message": "Mail Scheduled"}
--- project/facade/celery_service.py
+++ project/facade/celery_service.py
@@ -1,22 +1,31 @@
-from datetime import timedelta
+import datetime
+
 from celery import Celery
-from celery.schedules import crontab, solar
+from celery.schedules import crontab
 
 from .mail_service import send_mail
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+app = Celery("tasks", broker="pyamqp://guest@localhost//")
+
 
 @app.task
 def send_email_to_mom(origin, destiny, pwd, message):
     return send_mail(origin, destiny, pwd, message)
 
-# Testing Scheduling
-# app.conf.beat_schedule = {
-#     'add-every-30-seconds': {
-#         'task': 'celery.send_email_to_mom',
-#         'schedule': timedelta(seconds=5),
-#         'args': ()
-#     },
-# }
 
-app.conf.timezone = 'UTC'
+def schedule_service(origin, destiny, pwd, message, day, hour, minute):
+    time = datetime.datetime.utcnow() + datetime.timedelta(
+        days=day, hours=hour, minutes=minute
+    )
+    send_email_to_mom.apply_async((origin, destiny, pwd, message), eta=time)
+
+    # app.conf.beat_schedule = {
+    #     "add_weekly_schedule": {
+    #         "task": "celery.service.send_email_to_mom",
+    #         "schedule": crontab(day_of_week=day, hour=hour, minute=minute),
+    #         "args": (origin, destiny, pwd, message),
+    #     }
+    # }
+
+
+app.conf.timezone = "UTC"
